f50cc0f46ac4c6973f0913678c6536d65a8ac299
2363504 <cyycvdgytuseracc@gmail.com>
2024-04-22 15:13:24 -0400

    Added new sout in Main.java main()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a toy project");
+        System.out.println("Using Git Bash for VC");
     }
 }
